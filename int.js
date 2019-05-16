a22901e6776ce1b6f834af92167d9cd14f40a26a
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-05-16 14:58:14 +0000

    Automatically backed up by Learn

--- int.js
+++ int.js
@@ -1,6 +1,8 @@
+var arr = []
 function takeANumber (line){
   var queue = 1;
   line.push(queue)
   queue++;
-  console.log(`Welcome, you are number ${queue} in line.`)
+  console.log(`Welcome, you are number ` + (queue-1) ` in line.`)
 }
+
